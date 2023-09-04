ba39a1c8939d126cbb2c57397ed7aac672e4771d
Sascha Kalab <sascha.kalab@gmail.com>
2023-09-04 20:12:25 +0200

    initial commit

---
+++ header.h
@@ -0,0 +1,12 @@
+//
+// Created by Sascha Kalab on 02.09.23.
+//
+
+#ifndef CTUTORIAL_HEADER_H
+#define CTUTORIAL_HEADER_H
+
+int add(int a, int b) {
+    return a + b;
+}
+
+#endif //CTUTORIAL_HEADER_H
